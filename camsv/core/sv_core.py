8606375479ff2aad3933216538d3e32c83159849
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-27 16:42:18 -0300

    Trying stuff

--- camsv/core/sv_core.py
+++ camsv/core/sv_core.py
@@ -42,16 +42,27 @@ class ServerCore:
         self.__should_close = False
 
     def start(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.camera_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind(FIXED_SERVER_DESC)
-        self.camera_s.bind(FIXED_CAMERA_DESC)
-        self.s.listen()
+        # self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # self.camera_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # self.s.bind(FIXED_SERVER_DESC)
+        # self.camera_s.bind(FIXED_CAMERA_DESC)
+        # self.s.listen()
 
-        while not self.__should_close:
-            conn, addr = self.s.accept()
-            LOGGER.info(f"Connection accepted from address {addr}.")
-            conn.send(WELCOME_MSG.encode())
+        # while not self.__should_close:
+        #     conn, addr = self.s.accept()
+        #     LOGGER.info(f"Connection accepted from address {addr}.")
+        #     conn.send(WELCOME_MSG.encode())
 
-            user_thread = threading.Thread(target=handle_user, args=(conn, self.camera_s))
-            user_thread.start()
+        #     user_thread = threading.Thread(target=handle_user, args=(conn, self.camera_s))
+        #     user_thread.start()
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as camera_s:
+                s.bind(FIXED_SERVER_DESC)
+                camera_s.bind(FIXED_CAMERA_DESC)
+
+                while not self.__should_close:
+                    s.send(WELCOME_MSG.encode())
+
+                    user_thread = threading.Thread(target=handle_user, args=(s, camera_s))
+                    user_thread.start()
