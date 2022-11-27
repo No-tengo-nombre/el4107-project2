1f29e3f5c93057b04f4593971ac1b849bfd89765
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-27 17:10:20 -0300

    Started adding user validation

--- camsv/core/user_handle.py
+++ camsv/core/user_handle.py
@@ -1,6 +1,15 @@
 from camsv.configs import RECEIVING_WINDOW
 
 
-def handle_user(user_conn, camera_socket):
+def handle_user(user_conn, user_socket, camera_socket):
+    validate_user(user_conn)
+
     recv_packet = camera_socket.recv(RECEIVING_WINDOW)
     user_conn.send(recv_packet)
+
+
+def validate_user(conn):
+    conn.send("Enter username: ".encode())
+    username = conn.recv(1024).decode()
+    conn.send("Enter password: ".encode())
+    password = conn.recv(1024).decode()
