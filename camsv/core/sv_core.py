a6c7b021e63016b47ef47528aa4bf84cfad75527
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-27 16:11:10 -0300

    Changed the sockets to UDP sockets and passed the camera socket to the function

--- camsv/core/sv_core.py
+++ camsv/core/sv_core.py
@@ -42,8 +42,8 @@ class ServerCore:
         self.__should_close = False
 
     def start(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.camera_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.camera_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.s.bind(FIXED_SERVER_DESC)
         self.camera_s.bind(FIXED_CAMERA_DESC)
         self.s.listen()
@@ -53,5 +53,5 @@ class ServerCore:
             LOGGER.info(f"Connection accepted from address {addr}.")
             conn.send(WELCOME_MSG.encode())
 
-            user_thread = threading.Thread(target=handle_user, args=())
+            user_thread = threading.Thread(target=handle_user, args=(self.camera_s))
             user_thread.start()
