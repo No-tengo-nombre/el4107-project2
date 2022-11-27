09754fc1dc40db6ab47d74fe87d44239781efeca
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-27 16:30:41 -0300

    Made the packets redirect from the camera socket to the user socket

--- camsv/core/user_handle.py
+++ camsv/core/user_handle.py
@@ -1,2 +1,6 @@
-def handle_user(camera_socket):
-    pass
+from camsv.configs import RECEIVING_WINDOW
+
+
+def handle_user(user_conn, camera_socket):
+    recv_packet = camera_socket.recv(RECEIVING_WINDOW)
+    user_conn.send(recv_packet)
