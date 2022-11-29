a2f2eeccd2e68594f13ea77cf15cee9797910546
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-29 12:41:05 -0300

    Started working on the client code

--- src/camclient/core/client_core.py
+++ src/camclient/core/client_core.py
@@ -1,6 +1,18 @@
+import socket
+
+from camcommon import FIXED_SERVER_DESC
+from camcommon.logger import LOGGER
+
+
 class ClientCore:
     def __init__(self):
-        pass
+        self.__should_close = False
 
     def start(self):
-        pass
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(FIXED_SERVER_DESC)
+            s.listen()
+
+            while not self.__should_close:
+                conn, addr = s.accept()
+                LOGGER.info(f"Connection accepted from {addr}")
