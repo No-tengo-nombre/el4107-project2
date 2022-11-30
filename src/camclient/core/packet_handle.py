64888ab8457e578efcea8313a96e004c14c35ba6
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-30 13:45:06 -0300

    Added different handling according to authentication

--- src/camclient/core/packet_handle.py
+++ src/camclient/core/packet_handle.py
@@ -1,15 +1,20 @@
-from camcommon import RECEIVING_WINDOW, END_AUTH_STRING
+from camcommon import RECEIVING_WINDOW, END_AUTH_SUCCESS_STRING, END_AUTH_FAILURE_STRING
 
 
 def handle_auth(sv_socket):
     while True:
         packet = sv_socket.recv(RECEIVING_WINDOW)
-        if packet.decode() == END_AUTH_STRING:
+        if packet.decode() == END_AUTH_SUCCESS_STRING:
             break
-        print(packet.decode(), end="")
+        elif packet.decode() == END_AUTH_FAILURE_STRING:
+            packet = sv_socket.recv(RECEIVING_WINDOW)
+            print(packet.decode())
+            break
+
+        print(packet.decode())
+        print(">>> ", end="")
         sv_socket.send(input().encode())
 
 
-def handle_packet(packet):
-    print(packet)
-    print(packet.decode())
+def handle_packet(packet, client):
+    client.close()
