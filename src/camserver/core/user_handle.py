5739578a4fcac8fac90cfb0e320277f0dc25050c
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-30 13:46:52 -0300

    Added validation

--- src/camserver/core/user_handle.py
+++ src/camserver/core/user_handle.py
@@ -1,15 +1,21 @@
-from camcommon import RECEIVING_WINDOW
+from camcommon import RECEIVING_WINDOW, END_AUTH_STRING
 from camcommon.logger import LOGGER
 from camserver.database.database import UserNotFoundException
 
 
 def handle_user(db, user_conn, user_socket, camera_socket):
     if validate_user(db, user_conn):
+        LOGGER.info("Successfuly validated user.")
         user_conn.send("Successfuly validated user.".encode())
         recv_packet = camera_socket.recv(RECEIVING_WINDOW)
         user_conn.send(recv_packet)
     else:
+        LOGGER.info("Failed to validate user.")
+        user_conn.send(END_AUTH_STRING.encode())
+
         user_conn.send("Failed to validate user.".encode())
+        user_socket.close()
+        LOGGER.info("Finishing user thread.")
 
 
 def validate_user(db, conn):
@@ -26,7 +32,7 @@ def validate_user(db, conn):
         LOGGER.info(f"Validation status {validation_status}.")
         return validation_status
     except UserNotFoundException:
-        conn.send(f"User {username} was not found, would you like to create it? (y/n)".encode())
+        conn.send(f"User {username} was not found, would you like to create it? (y/n): ".encode())
         answer = conn.recv(1024).decode().upper()
         if answer == "Y":
             db.register_user(username, password)
