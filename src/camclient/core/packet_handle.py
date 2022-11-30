d2f11d24bf2bbc04f25139bc738d88c511223e71
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-30 19:04:26 -0300

    Added server commands

--- src/camclient/core/packet_handle.py
+++ src/camclient/core/packet_handle.py
@@ -1,4 +1,5 @@
 from camcommon import RECEIVING_WINDOW, END_AUTH_SUCCESS_STRING, END_AUTH_FAILURE_STRING
+from getpass import getpass
 
 
 def handle_auth(sv_socket):
@@ -11,10 +12,37 @@ def handle_auth(sv_socket):
             print("Failed to validate user :(")
             break
 
-        print(packet.decode())
-        print(">>> ", end="")
-        sv_socket.send(input().encode())
+        handle_command(packet.decode(), server_socket=sv_socket)
+
+
+def handle_command(cmd, server_socket):
+    action, *args = cmd.split(" ")
+    if action[0] == "@":
+        globals()[f"__token_action_{action[1:]}"](args, server_socket=server_socket)
 
 
 def handle_packet(packet, client):
     client.close()
+
+
+# Available commands
+# ==================
+# echo ARGS         -> Print to the user's console.
+# input ARGS        -> Print ARGS and get user input.
+# hidden_input ARGS -> Print ARGS and get user input,
+#                      without showing input to console.
+
+
+def __token_action_echo(args, server_socket):
+    print(*args)
+
+
+def __token_action_input(args, server_socket):
+    print(*args)
+    print(">>> ", end="")
+    server_socket.send(input().encode())
+
+
+def __token_action_hidden_input(args, server_socket):
+    print(*args)
+    server_socket.send(getpass(">>> ").encode())
