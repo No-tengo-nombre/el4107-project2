41b087f37f3baff982cf99a2b9bf54cc76c8f991
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-30 21:15:42 -0300

    Added port redirection

--- src/camserver/core/sv_core.py
+++ src/camserver/core/sv_core.py
@@ -1,10 +1,11 @@
 import socket
 import threading
 
-from camcommon import FIXED_CAMERA_DESC, FIXED_CAMERA_PORT, FIXED_SERVER_DESC, FIXED_SERVER_PORT
+from camcommon import FIXED_CAMERA_DESC, FIXED_SERVER_IP, FIXED_SERVER_DESC
 from camcommon.logger import LOGGER
 from camserver.core.user_handle import handle_user
 from camserver.database.database import USER_DATABASE
+from camserver.core.resource_assigner import PortAssigner
 
 
 WELCOME_MSG = r"""
@@ -48,14 +49,26 @@ class ServerCore:
         while not self.__should_close:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as camera_s:
-                    s.bind(FIXED_SERVER_DESC)
-                    camera_s.bind(FIXED_CAMERA_DESC)
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_s:
+                        s.bind(FIXED_SERVER_DESC)
+                        camera_s.bind(FIXED_CAMERA_DESC)
 
-                    s.listen()
-                    LOGGER.info(f"Waiting for a connection, socket {s}")
-                    conn, addr = s.accept()
-                    LOGGER.info(f"Connection accepted from address {addr}")
-                    conn.send(WELCOME_MSG.encode())
+                        # Receive the initial connection
+                        s.listen()
+                        LOGGER.info(f"Waiting for a connection, socket {s}")
+                        conn, addr = s.accept()
+                        LOGGER.info(f"Connection with {conn} accepted")
 
-                    user_thread = threading.Thread(target=handle_user, args=(self.db, conn, s, camera_s))
-                    user_thread.start()
+                        # Redirect the connection to another port
+                        port = PortAssigner.get_port()
+                        client_s.bind((FIXED_SERVER_IP, port))
+                        client_s.listen()
+                        conn.send(f"@redirect_port {port}".encode())
+
+                        # Accept the connection to the new port
+                        conn, addr = client_s.accept()
+                        LOGGER.info(f"Assigned {addr} to port {port}")
+                        conn.send(WELCOME_MSG.encode())
+
+                        user_thread = threading.Thread(target=handle_user, args=(self.db, conn, client_s, camera_s, port))
+                        user_thread.start()
