3715526e8cda6bc6a8eaa919938eb3598c09f606
Cristobal Allendes <callendes.molina@gmail.com>
2022-11-30 22:25:58 -0300

    Added properties for the client IP and port

--- src/camclient/core/client_core.py
+++ src/camclient/core/client_core.py
@@ -1,12 +1,42 @@
 import socket
 
-from camcommon import FIXED_SERVER_DESC, FIXED_SERVER_IP, RECEIVING_WINDOW
+from camcommon import FIXED_SERVER_DESC, FIXED_SERVER_IP, FIXED_SERVER_PORT, RECEIVING_WINDOW
 from camclient.core.packet_handle import handle_packet, handle_auth, handle_reconnection
 
 
 class ClientCore:
-    def __init__(self):
+    def __init__(self, ip=FIXED_SERVER_IP, port=FIXED_SERVER_PORT):
         self.__should_close = False
+        self._ip = ip
+        self._port = port
+        self._desc = (self.ip, self.port)
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def desc(self):
+        return self._desc
+
+    @ip.setter
+    def ip(self, new_ip):
+        self._ip = new_ip
+        self._desc = (new_ip, self.port)
+
+    @port.setter
+    def port(self, new_port):
+        self._port = new_port
+        self._desc = (self.ip, new_port)
+
+    @desc.setter
+    def desc(self, new_desc):
+        self._desc = new_desc
+        self._ip, self._port = new_desc
 
     def close(self):
         self.__should_close = True
@@ -15,7 +45,7 @@ class ClientCore:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_s:
                 # Accept the connection and redirect the port
-                s.connect(FIXED_SERVER_DESC)
+                s.connect(self.desc)
                 handle_reconnection(s, client_s)
 
                 # Receive the welcome message
