f2acd70215c357f5509c2a1e7898172c339eb55e
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 04:22:55 -0300

    Commited to the change in the way message ends are treated to be the same as the HTTP message end

--- src/cam_common/utils.py
+++ src/cam_common/utils.py
@@ -1,6 +1,6 @@
 import socket
 
-from cam_common.configs import RECEIVING_WINDOW, SIZE_RECEIVING_WINDOW
+from cam_common.configs import RECEIVING_WINDOW
 from cam_common.logger import LOGGER
 
 
@@ -18,47 +18,29 @@ class EMPTY_SOCKET:
         pass
 
 
-def yield_full_msg(sock, msg_size, window=RECEIVING_WINDOW):
-    recv_size = 0
-    while recv_size < msg_size:
+def yield_full_msg(sock, window=RECEIVING_WINDOW):
+    finished = False
+    while not finished:
         try:
             msg = sock.recv(window)
-            if msg.endswith(br"\r\n"):
-                recv_size = msg_size
+            if msg.endswith(rb"\r\n\r\n"):
+                finished = True
+                yield msg[:-8]
             else:
-                recv_size += window
-            yield msg
+                yield msg
         except socket.timeout:
             LOGGER.warning("Receiving timeout")
 
 
-def receive_full_msg(sock, size_window=SIZE_RECEIVING_WINDOW, window=RECEIVING_WINDOW):
-    # msg_size = int(sock.recv(size_window).decode())
-    # LOGGER.debug(f"Received message size {msg_size}")
-    # final_msg = b""
-    # for part in yield_full_msg(sock, msg_size, window):
-    #     final_msg += part
-    # LOGGER.debug(f"Received message {final_msg}")
-    # return final_msg
-
+def receive_full_msg(sock, window=RECEIVING_WINDOW):
     final_msg = b""
-    while True:
-        msg = sock.recv(window)
-        if msg.endswith(rb"\r\n\r\n"):
-            final_msg += msg[:-8]
-            break
-        else:
-            final_msg += msg
+    for part in yield_full_msg(sock, window):
+        final_msg += part
     LOGGER.debug(f"Received message {final_msg}")
     return final_msg
 
 
-def send_full_msg(sock, message, size_window=SIZE_RECEIVING_WINDOW):
-    """Send the message size and message, assumming the input is already encoded."""
-    # msg_size = len(message)
-    # sock.send(str(msg_size).zfill(size_window).encode())
-    # sock.send(message)
-
+def send_full_msg(sock, message):
     if message.endswith(rb"\r\n\r\n"):
         sock.send(message)
     else:
