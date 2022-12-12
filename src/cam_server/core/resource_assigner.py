068878a85bf4a734b5e1a998595d42dc639483c2
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 15:11:29 -0300

    Made methods to map sockets, ports and addresses inside the port assignet

--- src/cam_server/core/resource_assigner.py
+++ src/cam_server/core/resource_assigner.py
@@ -5,6 +5,8 @@ from cam_common.logger import LOGGER
 class PortAssigner:
     __free_ports = PORTS_FOR_USERS.copy()
     __used_ports = []
+    __socket_mapping = {}
+    __port_mapping = {}
 
     @staticmethod
     def get_port():
@@ -23,12 +25,28 @@ class PortAssigner:
         try:
             idx = PortAssigner.__used_ports.index(port)
             PortAssigner.__free_ports.append(PortAssigner.__used_ports.pop(idx))
+            _, addr = PortAssigner.__port_mapping[port]
+            del PortAssigner.__socket_mapping[addr]
+            del PortAssigner.__port_mapping[port]
             LOGGER.info(
                 f"Released port {port}, free ports {PortAssigner.__free_ports}, used ports {PortAssigner.__used_ports}"
             )
             return True
         except ValueError:
             return False
+
+    @staticmethod
+    def assign_socket_to_port(port, sock, addr):
+        PortAssigner.__socket_mapping[addr] = (sock, port)
+        PortAssigner.__port_mapping[port] = (sock, addr)
+
+    @staticmethod
+    def get_sock(port):
+        return PortAssigner.__port_mapping[port]
+
+    @staticmethod
+    def get_port(addr):
+        return PortAssigner.__socket_mapping[addr]
 
     def __enter__(self):
         self.__port = PortAssigner.get_port()
