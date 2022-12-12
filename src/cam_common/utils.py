5181e79ad106b7a8528e46ea7fddf1a064039e98
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 00:04:00 -0300

    Added another function to return the full messgae

--- src/cam_common/utils.py
+++ src/cam_common/utils.py
@@ -1,9 +1,16 @@
 from cam_common.configs import RECEIVING_WINDOW
 
 
-def receive_full_msg(sock, window=RECEIVING_WINDOW):
+def yield_full_msg(sock, window=RECEIVING_WINDOW):
     while True:
         msg = sock.recv(window).decode()
         if msg == "":
             break
         yield msg
+
+
+def receive_full_msg(sock, window=RECEIVING_WINDOW):
+    final_msg = ""
+    for part in yield_full_msg(sock, window):
+        final_msg += part
+    return final_msg
