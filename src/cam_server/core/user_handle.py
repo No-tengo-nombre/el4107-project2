f1835a4198d87edd561ea981d1aa70766d09f64d
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 01:53:18 -0300

    Changed the initial wait

--- src/cam_server/core/user_handle.py
+++ src/cam_server/core/user_handle.py
@@ -8,7 +8,7 @@ import time
 
 def handle_user(server, db, user_conn, user_socket, client_conn, client_socket, user_port):
     try:
-        time.sleep(3)
+        time.sleep(0.1)
         if validate_user(db, user_conn):
             LOGGER.info("Successfuly validated user")
             send_full_msg(user_conn, "@echo Successfuly validated user :)".encode())
