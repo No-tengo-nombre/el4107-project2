abfc1a87ce7a135e5cb49c3b49436ad564204cb2
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 00:27:08 -0300

    Wrapped the receive in a timeout check and added a class for an empty socket

--- src/cam_common/utils.py
+++ src/cam_common/utils.py
@@ -1,16 +1,38 @@
+import socket
+
 from cam_common.configs import RECEIVING_WINDOW
+from cam_common.logger import LOGGER
+
+
+class EMPTY_SOCKET:
+    @staticmethod
+    def settimeout(*_):
+        pass
+
+    @staticmethod
+    def recv(*_):
+        pass
+
+    @staticmethod
+    def send(*_):
+        pass
 
 
 def yield_full_msg(sock, window=RECEIVING_WINDOW):
     while True:
-        msg = sock.recv(window)
-        if msg == b"":
-            break
-        yield msg
+        try:
+            msg = sock.recv(window)
+            if msg == b"":
+                break
+            yield msg
+        except socket.timeout:
+            LOGGER.warning("Receiving timeout")
 
 
 def receive_full_msg(sock, window=RECEIVING_WINDOW):
     final_msg = b""
     for part in yield_full_msg(sock, window):
+        print(part)
         final_msg += part
+    print("SDFSFSDF")
     return final_msg
