f1c9a45de773efdaff6c0d6a54cdc3f99b33fe60
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 03:40:13 -0300

    Made calls to the target be sent and received without headers

--- src/cam_client/core/proxy_handle.py
+++ src/cam_client/core/proxy_handle.py
@@ -1,3 +1,4 @@
+from cam_common.configs import RECEIVING_WINDOW
 from cam_common.logger import LOGGER
 from cam_common.utils import receive_full_msg, send_full_msg
 
@@ -7,8 +8,10 @@ def handle_information_flow(server_socket, target_socket):
     LOGGER.debug("Listening for server packet")
     packet = receive_full_msg(server_socket)
     LOGGER.debug("Sending packet to target")
-    send_full_msg(target_socket, packet)
+    # send_full_msg(target_socket, packet)
+    target_socket.send(packet)
     LOGGER.debug("Listening for target packet")
-    response = receive_full_msg(target_socket)
+    # response = receive_full_msg(target_socket)
+    response = target_socket.recv(RECEIVING_WINDOW)
     LOGGER.debug("Sending packet to server")
     send_full_msg(server_socket, response)
