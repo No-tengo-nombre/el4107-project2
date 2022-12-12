cd55a88166a9b8588362982e5ecccd9a811c957e
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 00:57:30 -0300

    Changed the way sending and receiving is made

--- src/cam_common/utils.py
+++ src/cam_common/utils.py
@@ -18,21 +18,42 @@ class EMPTY_SOCKET:
         pass
 
 
-def yield_full_msg(sock, window=RECEIVING_WINDOW):
-    while True:
+def yield_full_msg(sock, msg_size, window=RECEIVING_WINDOW):
+    # while True:
+    #     try:
+    #         msg = sock.recv(window)
+    #         if msg == b"":
+    #             break
+    #         yield msg
+    #     except socket.timeout:
+    #         LOGGER.warning("Receiving timeout")
+
+    recv_size = 0
+    while recv_size < msg_size:
         try:
             msg = sock.recv(window)
-            if msg == b"":
-                break
+            recv_size += window
             yield msg
         except socket.timeout:
             LOGGER.warning("Receiving timeout")
 
 
 def receive_full_msg(sock, window=RECEIVING_WINDOW):
-    final_msg = b""
-    for part in yield_full_msg(sock, window):
-        print(part)
+    # final_msg = b""
+    # for part in yield_full_msg(sock, window):
+    #     print(part)
+    #     final_msg += part
+    # print("SDFSFSDF")
+    # return final_msg
+
+    msg_size = int(sock.recv(window).decode())
+    for part in yield_full_msg(sock, msg_size, window):
         final_msg += part
-    print("SDFSFSDF")
     return final_msg
+
+
+def send_full_msg(sock, message, window=RECEIVING_WINDOW):
+    """Send the message size and message, assumming the input is already encoded."""
+    msg_size = len(message)
+    sock.send(str(msg_size).encode())
+    sock.send(message)
