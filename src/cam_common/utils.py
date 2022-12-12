627fa218062f238fc02f1c842ff77d326120ea0a
Cristobal Allendes <callendes.molina@gmail.com>
2022-12-12 04:10:00 -0300

    Trying something

--- src/cam_common/utils.py
+++ src/cam_common/utils.py
@@ -33,17 +33,31 @@ def yield_full_msg(sock, msg_size, window=RECEIVING_WINDOW):
 
 
 def receive_full_msg(sock, size_window=SIZE_RECEIVING_WINDOW, window=RECEIVING_WINDOW):
-    msg_size = int(sock.recv(size_window).decode())
-    LOGGER.debug(f"Received message size {msg_size}")
+    # msg_size = int(sock.recv(size_window).decode())
+    # LOGGER.debug(f"Received message size {msg_size}")
+    # final_msg = b""
+    # for part in yield_full_msg(sock, msg_size, window):
+    #     final_msg += part
+    # LOGGER.debug(f"Received message {final_msg}")
+    # return final_msg
+
     final_msg = b""
-    for part in yield_full_msg(sock, msg_size, window):
-        final_msg += part
+    while True:
+        msg = sock.recv(window)
+        final_msg += msg
+        if msg.endswith(rb"\r\n\r\n"):
+            break
     LOGGER.debug(f"Received message {final_msg}")
     return final_msg
 
 
 def send_full_msg(sock, message, size_window=SIZE_RECEIVING_WINDOW):
     """Send the message size and message, assumming the input is already encoded."""
-    msg_size = len(message)
-    sock.send(str(msg_size).zfill(size_window).encode())
-    sock.send(message)
+    # msg_size = len(message)
+    # sock.send(str(msg_size).zfill(size_window).encode())
+    # sock.send(message)
+
+    if message.endswith(rb"\r\n\r\n"):
+        sock.send(message)
+    else:
+        sock.send(message + br"\r\n\r\n")
